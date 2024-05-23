952c0eaa67fb667f0a7a3adb783c330f342e274f
madddmax <madddmax@mail.ru>
2024-05-23 22:36:48 +0300

    Replaced MSTests vs xUnit

---
+++ Jackal.Tests2/JsonTests.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+
+namespace Jackal.Tests2
+{
+    public class JsonTests
+    {
+        [Fact]
+        public void JsonJackalObjects()
+        {
+            CheckSerialization(new Position(1, 2),true);
+            CheckSerialization(new Direction(new TilePosition(new Position(1, 2)), new TilePosition(new Position(3, 4))));
+            CheckSerialization(new TilePosition( new Position(1, 2),3), true);
+        }
+
+        private static void CheckSerialization<T>(T obj,bool print=false) where T : class
+        {
+            var json = JsonHelper.SerialiazeWithType<T>(obj, Formatting.Indented);
+            if (print)
+            {
+                Console.WriteLine(obj.GetType().Name);
+                Console.WriteLine(json);
+            }
+            var obj2 = JsonHelper.DeserialiazeWithType<T>(json);
+            Assert.NotNull(obj2);
+            var json2 = JsonHelper.SerialiazeWithType<T>(obj2, Formatting.Indented);
+            if (json != json2)
+            {
+                Console.WriteLine(json);
+                Console.WriteLine(json2);
+            }
+            Assert.True(json == json2);
+        }
+    }
+}
---
+++ Jackal.Tests2/UnitTests.cs
@@ -0,0 +1,40 @@
+namespace Jackal.Tests2;
+
+public class UnitTests
+{
+    [Fact]
+    public void TestPermutations()
+    {
+        Assert.True(Utils.Factorial(0) == 1);
+        Assert.True(Utils.Factorial(1) == 1);
+        Assert.True(Utils.Factorial(4) == 4 * 3 * 2 * 1);
+
+        Console.WriteLine("Permutations:");
+        HashSet<string> hashSet=new HashSet<string>();
+        for (int i = 0; i < Utils.Factorial(4); i++)
+        {
+            var rec = Utils.GetPermutation(i, new[] {"1", "2", "3", "4"});
+            var val = string.Join(",", rec.ToArray());
+            Assert.True(hashSet.Contains(val) == false);
+            Console.WriteLine("{0}: {1}", i, val);
+        }
+        var set1 = Utils.GetPermutation(0, new[] {"1", "2", "3", "4"});
+        var set2 = Utils.GetPermutation(Utils.Factorial(4), new[] {"1", "2", "3", "4"});
+        Assert.True(set1.SequenceEqual(set2));
+    }
+
+    [Fact]
+    public void TestArrowsHelper()
+    {
+        var rez = ArrowsCodesHelper.DoRotate(255);
+        Assert.True(rez==255);
+
+        for (int code = 0; code <= 255; code++)
+        {
+            int test = code;
+            for (int i = 1; i <= 4; i++)
+                test = ArrowsCodesHelper.DoRotate(test);
+            Assert.True(code == test);
+        }
+    }
+}
