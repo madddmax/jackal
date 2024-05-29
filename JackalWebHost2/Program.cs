cd8c57149f89948887b3c43bef85a87ece289ae7
madddmax <madddmax@mail.ru>
2024-05-29 12:27:29 +0300

    Turn on http

--- JackalWebHost2/Program.cs
+++ JackalWebHost2/Program.cs
@@ -9,7 +9,7 @@ namespace JackalWebHost2
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-            var config = builder.Configuration;
+            //var config = builder.Configuration;
 
             // Add services to the container.
             var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
@@ -48,10 +48,10 @@ namespace JackalWebHost2
             {
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                //app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
