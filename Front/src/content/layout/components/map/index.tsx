e2b4f75f74f023f3c28dc071bed1b587617967d8
LUCK\Mike <mifonrule@gmail.com>
2024-07-17 13:58:08 +0300

    refactoring

--- Front/src/content/layout/components/map/index.tsx
+++ Front/src/content/layout/components/map/index.tsx
@@ -1,26 +1,31 @@
 import { useSelector } from 'react-redux';
 import Cell from './cell';
-import './map.less'
+import './map.less';
 
 import { GameState, ReduxState } from '/redux/types';
 
 function Map() {
-
     const game = useSelector<ReduxState, GameState>((state) => state.game);
 
     return (
-      <div className='map'>
-        {game.fields.map((row, rIndex) => (
-          <div className='map-row' key={`map-row-${game.fields.length - 1 - rIndex}`}>
-            {row.map((_col, cIndex) => 
-              <div className='map-cell' key={`map-cell-${cIndex}`}>
-                <Cell col={cIndex} row={game.fields.length - 1 - rIndex} />
-              </div>)
-            }
-          </div>
-        ))}
-      </div>  
+        <div className="map">
+            {game.fields.map((row, rIndex) => (
+                <div
+                    className="map-row"
+                    key={`map-row-${game.fields.length - 1 - rIndex}`}
+                >
+                    {row.map((_col, cIndex) => (
+                        <div className="map-cell" key={`map-cell-${cIndex}`}>
+                            <Cell
+                                col={cIndex}
+                                row={game.fields.length - 1 - rIndex}
+                            />
+                        </div>
+                    ))}
+                </div>
+            ))}
+        </div>
     );
-  }
-  
-  export default Map;
+}
+
+export default Map;
