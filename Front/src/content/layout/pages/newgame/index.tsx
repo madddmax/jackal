e1b7995c9a6471c8cd0f0c9ba42b23e33df2c789
LUCK\Mike <mifonrule@gmail.com>
2024-07-19 15:57:34 +0300

    fix adding new game

--- Front/src/content/layout/pages/newgame/index.tsx
+++ Front/src/content/layout/pages/newgame/index.tsx
@@ -1,23 +1,59 @@
-import { useMemo } from 'react';
+import { useMemo, useState } from 'react';
 import { Button, Form } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import './newgame.css';
+import { useDispatch } from 'react-redux';
+import { sagaActions } from '/redux/saga';
+import { useNavigate } from 'react-router-dom';
 
 function Newgame() {
-    const human = '/pictures/human.png';
-    const robot = '/pictures/robot.png';
-    const robot2 = '/pictures/robot2.png';
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
 
-    const randomNumber = useMemo(
-        () => crypto.getRandomValues(new Uint32Array(1)),
-        [],
+    const [players, setPlayers] = useState([
+        'human',
+        'robot2',
+        'robot',
+        'robot2',
+    ]);
+
+    const [randNumber, setRandNumber] = useState(() =>
+        crypto.getRandomValues(new Uint32Array(1)),
     );
+
+    const getUrlByPlayer = (play: string) => {
+        if (play === 'human') return '/pictures/human.png';
+        else if (play === 'robot') return '/pictures/robot.png';
+        return '/pictures/robot2.png';
+    };
+
+    const changePlayer = (pos: number) => {
+        const clone = [...players];
+        if (clone[pos] === 'human') clone[pos] = 'robot';
+        else if (clone[pos] === 'robot') clone[pos] = 'robot2';
+        else clone[pos] = 'human';
+        setPlayers(clone);
+    };
+
+    const newStart = (event: React.FormEvent) => {
+        event.preventDefault();
+        console.log('values', players, randNumber);
+        navigate('/');
+        dispatch({
+            type: sagaActions.GAME_START,
+            payload: {
+                gameName: 'afc9847e-dce9-497d-bac8-767c3d571b48',
+                settings: JSON.stringify({ players, mapId: randNumber[0] }),
+            },
+        });
+    };
 
     return (
         <Container>
             <Row className="justify-content-center">
                 <Form
+                    onSubmit={(event) => newStart(event)}
                     style={{
                         width: '100%',
                         maxWidth: '500px',
@@ -30,43 +66,58 @@ function Newgame() {
                     <div className="settings">
                         <div
                             className="player"
+                            onClick={() => changePlayer(0)}
                             style={{
                                 top: '200px',
                                 left: '100px',
-                                backgroundImage: `url(${human})`,
+                                backgroundImage: `url(${getUrlByPlayer(players[0])})`,
                             }}
                         ></div>
                         <div
                             className="player"
+                            onClick={() => changePlayer(1)}
                             style={{
                                 top: '100px',
                                 left: '0px',
-                                backgroundImage: `url(${robot2})`,
+                                backgroundImage: `url(${getUrlByPlayer(players[1])})`,
                             }}
                         ></div>
                         <div
                             className="player"
+                            onClick={() => changePlayer(2)}
                             style={{
                                 top: '0px',
                                 left: '100px',
-                                backgroundImage: `url(${robot})`,
+                                backgroundImage: `url(${getUrlByPlayer(players[2])})`,
                             }}
                         ></div>
                         <div
                             className="player"
+                            onClick={() => changePlayer(3)}
                             style={{
                                 top: '100px',
                                 left: '200px',
-                                backgroundImage: `url(${robot2})`,
+                                backgroundImage: `url(${getUrlByPlayer(players[3])})`,
                             }}
                         ></div>
                     </div>
+                    <Form.Control
+                        type="hidden"
+                        name="players"
+                        value={players}
+                    />
                     <Form.Group className="mb-3" controlId="formBasicEmail">
                         <Form.Label>Код карты</Form.Label>
                         <Form.Control
                             type="text"
+                            name="mapcode"
                             placeholder="Введите код"
-                            value={randomNumber[0]}
+                            value={randNumber[0]}
+                            onChange={(event) => {
+                                let clone = new Uint32Array(1);
+                                clone[0] = Number(event.target.value);
+                                setRandNumber(clone);
+                            }}
                         />
                     </Form.Group>
                     <Button variant="primary" type="submit">
