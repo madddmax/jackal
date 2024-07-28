c2c11568483bff53a48269c0f765dbbdacbc4647
LUCK\Mike <mifonrule@gmail.com>
2024-07-28 23:48:07 +0300

    новые действия с монетой

--- Front/src/content/layout/pages/playground/components/pirates/index.tsx
+++ Front/src/content/layout/pages/playground/components/pirates/index.tsx
@@ -1,8 +1,6 @@
 import { useDispatch, useSelector } from 'react-redux';
-import cn from 'classnames';
-
-import Image from 'react-bootstrap/Image';
 import { highlightMoves } from '/redux/gameSlice';
+import Pirate from './pirate';
 import './pirates.css';
 import { ReduxState } from '/redux/types';
 
@@ -16,49 +14,37 @@ function Pirates() {
         (state) => state.game.withCoin,
     );
 
+    const onClick = (num: number) => () =>
+        dispatch(
+            highlightMoves({
+                pirate: num,
+                withCoin:
+                    activePirate !== num || withCoin === undefined
+                        ? undefined
+                        : !withCoin,
+            }),
+        );
+
     return (
         <>
-            <Image
-                src="/pictures/pirate_1.png"
-                roundedCircle
-                className={cn('photo float-end', {
-                    'photo-active': activePirate === 1,
-                })}
-                onClick={() => dispatch(highlightMoves({ pirate: 1 }))}
+            <Pirate
+                photo="/pictures/pirate_1.png"
+                isActive={activePirate === 1}
+                withCoin={activePirate === 1 ? withCoin : undefined}
+                onClick={onClick(1)}
             />
-            <Image
-                src="/pictures/pirate_2.png"
-                roundedCircle
-                className={cn('photo float-end', {
-                    'photo-active': activePirate === 2,
-                })}
-                onClick={() => dispatch(highlightMoves({ pirate: 2 }))}
+            <Pirate
+                photo="/pictures/pirate_2.png"
+                isActive={activePirate === 2}
+                withCoin={activePirate === 2 ? withCoin : undefined}
+                onClick={onClick(2)}
             />
-            <Image
-                src="/pictures/pirate_3.png"
-                roundedCircle
-                className={cn('photo float-end', {
-                    'photo-active': activePirate === 3,
-                })}
-                onClick={() => dispatch(highlightMoves({ pirate: 3 }))}
+            <Pirate
+                photo="/pictures/pirate_3.png"
+                isActive={activePirate === 3}
+                withCoin={activePirate === 3 ? withCoin : undefined}
+                onClick={onClick(3)}
             />
-            {withCoin !== undefined && (
-                <div className="form-check float-end">
-                    <input
-                        className="form-check-input"
-                        type="checkbox"
-                        value=""
-                        id="with-coin"
-                        checked={withCoin}
-                        onChange={() =>
-                            dispatch(highlightMoves({ withCoin: !withCoin }))
-                        }
-                    />
-                    <label className="form-check-label" htmlFor="with-coin">
-                        с монетой
-                    </label>
-                </div>
-            )}
         </>
     );
 }
