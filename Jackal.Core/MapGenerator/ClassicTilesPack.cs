b7108963b7f8dc11d4851a2c8c1c42de732ec524
madddmax <madddmax@mail.ru>
2024-09-13 21:50:38 +0300

    Refactor classic tiles pack

--- Jackal.Core/MapGenerator/ClassicTilesPack.cs
+++ Jackal.Core/MapGenerator/ClassicTilesPack.cs
@@ -7,71 +7,20 @@ namespace Jackal.Core
     /// </summary>
     public class ClassicTilesPack
     {
-        private readonly int TotalTiles;
+        private readonly int _totalTiles;
+        private readonly List<TileParams> _list;
+
+        public IReadOnlyList<TileParams> List => _list;
 
         public int CoinsOnMap { get; private set; }
         
         public ClassicTilesPack(int mapSize)
         {
             int landSize = mapSize - 2;
-            TotalTiles = landSize * landSize - 4;
+            _totalTiles = landSize * landSize - 4;
             
-            Fill();
-        }
-
-        private void AddArrowDef(int arrowsCode, int count)
-        {
-            for (int i = 1; i <= count; i++)
-            {
-                if (_list.Count == TotalTiles)
-                    return;
-
-                var def = new TileParams { Type = TileType.Arrow, ArrowsCode = arrowsCode };
-                _list.Add(def);
-            }
-        }
-
-        private void AddSpinningDef(int spirnningCount, int count)
-        {
-            for (int i = 1; i <= count; i++)
-            {
-                if (_list.Count == TotalTiles)
-                    return;
-
-                var def = new TileParams { Type = TileType.Spinning, SpinningCount = spirnningCount };
-                _list.Add(def);
-            }
-        }
-
-        private void AddDef(TileType tileType, int count = 1)
-        {
-            for (int i = 1; i <= count; i++)
-            {
-                if (_list.Count == TotalTiles)
-                    return;
-
-                UpdateCoinsOnMap(tileType);
-
-                var def = new TileParams { Type = tileType };
-                _list.Add(def);
-            }
-        }
-
-        private void UpdateCoinsOnMap(TileType tileType) =>
-            CoinsOnMap += tileType switch
-            {
-                TileType.Chest1 => 1,
-                TileType.Chest2 => 2,
-                TileType.Chest3 => 3,
-                TileType.Chest4 => 4,
-                TileType.Chest5 => 5,
-                _ => 0
-            };
-
-        private void Fill()
-        {
             CoinsOnMap = 0;
-            _list = new List<TileParams>(TotalTiles);
+            _list = new List<TileParams>(_totalTiles);
             
             AddDef(TileType.Chest1, 5);
             AddDef(TileType.Chest2, 5);
@@ -86,12 +35,10 @@ namespace Jackal.Core
             AddDef(TileType.Airplane, 2);
             AddDef(TileType.Crocodile, 4);
             AddDef(TileType.Ice, 6);
-			AddDef(TileType.Cannon, 2);
-
-            //arrows
-            for (int arrowType = 0; arrowType < ArrowsCodesHelper.ArrowsTypes.Length; arrowType++)
+            AddDef(TileType.Cannon, 2);
+            
+            foreach (var arrowsCode in ArrowsCodesHelper.ArrowsTypes)
             {
-                int arrowsCode = ArrowsCodesHelper.ArrowsTypes[arrowType];
                 AddArrowDef(arrowsCode, 3);
             }
             
@@ -105,20 +52,59 @@ namespace Jackal.Core
             AddSpinningDef(4, 2);
             AddSpinningDef(5, 1);
             
-            while (_list.Count < TotalTiles)
+            while (_list.Count < _totalTiles)
             {
                 AddDef(TileType.Grass);
             }
         }
 
-        private List<TileParams> _list;
-
-        public IReadOnlyList<TileParams> List
+        private void AddDef(TileType tileType, int count = 1)
         {
-            get
+            for (int i = 1; i <= count; i++)
             {
-                return _list;
+                if (_list.Count == _totalTiles)
+                    return;
+
+                UpdateCoinsOnMap(tileType);
+
+                var def = new TileParams { Type = tileType };
+                _list.Add(def);
             }
         }
+        
+        private void AddArrowDef(int arrowsCode, int count)
+        {
+            for (int i = 1; i <= count; i++)
+            {
+                if (_list.Count == _totalTiles)
+                    return;
+
+                var def = new TileParams { Type = TileType.Arrow, ArrowsCode = arrowsCode };
+                _list.Add(def);
+            }
+        }
+
+        private void AddSpinningDef(int spinningCount, int count)
+        {
+            for (int i = 1; i <= count; i++)
+            {
+                if (_list.Count == _totalTiles)
+                    return;
+
+                var def = new TileParams { Type = TileType.Spinning, SpinningCount = spinningCount };
+                _list.Add(def);
+            }
+        }
+
+        private void UpdateCoinsOnMap(TileType tileType) =>
+            CoinsOnMap += tileType switch
+            {
+                TileType.Chest1 => 1,
+                TileType.Chest2 => 2,
+                TileType.Chest3 => 3,
+                TileType.Chest4 => 4,
+                TileType.Chest5 => 5,
+                _ => 0
+            };
     }
 }
