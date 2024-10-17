0441a3ac616b7bed4363a28ff96c2efd1441bcd3
LUCK\Mike <mifonrule@gmail.com>
2024-10-18 00:07:24 +0300

    удаляем лишнее

--- Front/src/sagas/gameSaga.ts
+++ Front/src/sagas/gameSaga.ts
@@ -14,54 +14,46 @@ import { GameStartResponse, GameTurnResponse } from '../redux/types';
 import { errorsWrapper, sagaActions } from './constants';
 
 export function* gameReset() {
-    try {
-        let result: GameStartResponse = yield call(
-            async () =>
-                await axios({
-                    url: `${config.BaseApi}Game/Reset`,
-                    method: 'post',
-                }),
-        );
-        console.log(result);
-        // yield put(initMap(result.data.));
-    } catch (e) {
-        yield put({ type: 'TODO_FETCH_FAILED' });
-    }
+    let result: GameStartResponse = yield call(
+        async () =>
+            await axios({
+                url: `${config.BaseApi}Game/Reset`,
+                method: 'post',
+            }),
+    );
+    console.log(result);
+    // yield put(initMap(result.data.));
 }
 
 export function* gameStart(action: any) {
-    try {
-        let result: { data: GameStartResponse } = yield call(
-            async () =>
-                await axios({
-                    url: `${config.BaseApi}v1/game/start`,
-                    method: 'post',
-                    data: action.payload,
-                }),
-        );
-        yield put(initMap(result.data.map));
-        console.log('gameStart');
-        yield put(initGame(result.data));
-        yield put(
-            applyPirateChanges({
-                moves: result.data.moves,
-                changes: result.data.pirates,
-                isHumanPlayer: result.data.stats.isHumanPlayer,
+    let result: { data: GameStartResponse } = yield call(
+        async () =>
+            await axios({
+                url: `${config.BaseApi}v1/game/start`,
+                method: 'post',
+                data: action.payload,
             }),
-        );
-        if (result.data.stats.isHumanPlayer) {
-            yield put(setCurrentHumanTeam(result.data.stats.currentTeamId));
-            yield put(highlightHumanMoves({ moves: result.data.moves }));
-        }
-        yield put(applyStat(result.data.stats));
-        if (!result.data.stats.isHumanPlayer || result.data.moves?.length == 0) {
-            yield call(gameTurn, {
-                type: sagaActions.GAME_TURN,
-                payload: { gameName: result.data.gameName },
-            });
-        }
-    } catch (e) {
-        yield put({ type: 'TODO_FETCH_FAILED' });
+    );
+    yield put(initMap(result.data.map));
+    console.log('gameStart');
+    yield put(initGame(result.data));
+    yield put(
+        applyPirateChanges({
+            moves: result.data.moves,
+            changes: result.data.pirates,
+            isHumanPlayer: result.data.stats.isHumanPlayer,
+        }),
+    );
+    if (result.data.stats.isHumanPlayer) {
+        yield put(setCurrentHumanTeam(result.data.stats.currentTeamId));
+        yield put(highlightHumanMoves({ moves: result.data.moves }));
+    }
+    yield put(applyStat(result.data.stats));
+    if (!result.data.stats.isHumanPlayer || result.data.moves?.length == 0) {
+        yield call(gameTurn, {
+            type: sagaActions.GAME_TURN,
+            payload: { gameName: result.data.gameName },
+        });
     }
 }
 
@@ -73,38 +65,34 @@ export function* gameTurn(action: any) {
 }
 
 export function* oneTurn(action: any) {
-    try {
-        let result: { data: GameTurnResponse } = yield call(
-            async () =>
-                await axios({
-                    url: `${config.BaseApi}v1/game/move`,
-                    method: 'post',
-                    data: action.payload,
-                }),
-        );
-        if (result.data.stats.isGameOver) {
-            yield put(applyStat(result.data.stats));
-            return false;
-        }
-        console.log('gameTurn');
-        yield put(applyChanges(result.data.changes));
-        yield put(
-            applyPirateChanges({
-                moves: result.data.moves,
-                changes: result.data.pirateChanges,
-                isHumanPlayer: result.data.stats.isHumanPlayer,
+    let result: { data: GameTurnResponse } = yield call(
+        async () =>
+            await axios({
+                url: `${config.BaseApi}v1/game/move`,
+                method: 'post',
+                data: action.payload,
             }),
-        );
-        if (result.data.stats.isHumanPlayer) {
-            yield put(setCurrentHumanTeam(result.data.stats.currentTeamId));
-            yield put(highlightHumanMoves({ moves: result.data.moves }));
-        }
+    );
+    if (result.data.stats.isGameOver) {
         yield put(applyStat(result.data.stats));
-
-        return !result.data.stats.isHumanPlayer || result.data.moves?.length == 0;
-    } catch (e) {
-        yield put({ type: 'TODO_FETCH_FAILED' });
+        return false;
     }
+    console.log('gameTurn');
+    yield put(applyChanges(result.data.changes));
+    yield put(
+        applyPirateChanges({
+            moves: result.data.moves,
+            changes: result.data.pirateChanges,
+            isHumanPlayer: result.data.stats.isHumanPlayer,
+        }),
+    );
+    if (result.data.stats.isHumanPlayer) {
+        yield put(setCurrentHumanTeam(result.data.stats.currentTeamId));
+        yield put(highlightHumanMoves({ moves: result.data.moves }));
+    }
+    yield put(applyStat(result.data.stats));
+
+    return !result.data.stats.isHumanPlayer || result.data.moves?.length == 0;
 }
 
 export default function* rootSaga() {
