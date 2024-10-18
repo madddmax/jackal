c81c93923e95f967c135cb764d525673568ab6a2
LUCK\Mike <mifonrule@gmail.com>
2024-10-18 22:53:02 +0300

    Revert "all configs"
    This reverts commit 710122d2935a9e409a2480a4da2c7a0ff1d468fc.

--- Front/src/app/config.ts
+++ Front/src/app/config.ts
@@ -1,7 +1,7 @@
 let config = {
-    BaseApi: 'http://116.203.101.2/api/',
+    BaseApi: "http://localhost:5130/api/",
     HasDebug: true,
-};
+}
 if (process.env.NODE_ENV && process.env.NODE_ENV === 'production') {
     // production code
     config = {
