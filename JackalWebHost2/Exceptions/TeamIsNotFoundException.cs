bda0ff8e4b8febc23ef325f57f461b2568d77acc
dlepeshkin <daniil.h1233@gmail.com>
2024-11-09 12:48:29 +0300

    Added new methods to LobbyService

---
+++ JackalWebHost2/Exceptions/TeamIsNotFoundException.cs
@@ -0,0 +1,8 @@
+﻿namespace JackalWebHost2.Exceptions;
+
+public class TeamIsNotFoundException : BusinessException
+{
+    public override string ErrorMessage => "Выбранная команда не найдена";
+
+    public override string ErrorCode => ErrorCodes.TeamIsNotFound;
+}
