dd684d63db7c380de06c1a054a6d9c2b9ad908b2
LUCK\Mike <mifonrule@gmail.com>
2024-11-17 10:06:19 +0300

    fix auth on prod

--- Front/src/app/config.ts
+++ Front/src/app/config.ts
@@ -1,11 +1,16 @@
-let config = {
-    BaseApi: "http://localhost:5130/api/",
-    HasDebug: true,
+export interface ConfigAttributes {
+    BaseApi: string;
+    HasDebug: boolean;
 }
+
+let config: ConfigAttributes = {
+    BaseApi: 'http://localhost:5130/api/',
+    HasDebug: true,
+};
 if (process.env.NODE_ENV && process.env.NODE_ENV === 'production') {
     // production code
     config = {
-        BaseApi: 'https://jackal.team/api/',
+        BaseApi: window.location.origin + '/api',
         HasDebug: false,
     };
 }
