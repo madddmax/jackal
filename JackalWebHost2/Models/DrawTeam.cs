da16a291bdc4aab3906599fe743bfa08f5e690b9
madddmax <madddmax@mail.ru>
2025-03-09 21:22:24 +0300

    Добавил IsHuman в Api Teams бэке

--- JackalWebHost2/Models/DrawTeam.cs
+++ JackalWebHost2/Models/DrawTeam.cs
@@ -6,6 +6,7 @@ public class DrawTeam(Team team)
 {
     public int Id = team.Id;
     public string Name = team.Name;
+    public bool IsHuman = team.Name == "WebHumanPlayer";
     public int Coins = team.Coins;
     public DrawPosition Ship = new(team.ShipPosition);
 }
