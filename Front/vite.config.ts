7bf92df87d9c267e459f5680f6b21961ad23e582
LUCK\Mike <mifonrule@gmail.com>
2025-03-23 11:32:10 +0300

    fix tests

--- Front/vite.config.ts
+++ Front/vite.config.ts
@@ -1,13 +1,21 @@
 import react from '@vitejs/plugin-react-swc';
 import { fileURLToPath } from 'node:url';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
     const isProd = mode === 'production';
+    const env = loadEnv(mode, process.cwd(), '');
 
     return {
         plugins: [react()],
+        define: {
+            'process.env': {
+                NODE_ENV: env.NODE_ENV,
+                BASE_API: env.BASE_API,
+                HUB_API: env.HUB_API,
+            },
+        },
         build: {
             outDir: isProd ? 'dist' : 'dev',
             assetsDir: isProd ? 'dist' : 'dev',
