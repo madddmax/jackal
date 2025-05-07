f950cf5b0046379344a2f5a7cdd5ea5400895555
LUCK\Mike <mifonrule@gmail.com>
2025-05-07 08:52:51 +0300

    проставил поля, которые нужно заполнить Максу

--- JackalWebHost2/Data/Entities/GameCacheEntry.cs
+++ JackalWebHost2/Data/Entities/GameCacheEntry.cs
@@ -4,8 +4,19 @@
     {
         public long GameId { get; init; }
 
+        [Obsolete("Заменить на Creator")]
         public long CreatorId { get; init; }
 
+        public GameCacheEntryCreator Creator { get; init; } = new GameCacheEntryCreator{ Name = "Неизвестно" };
+
         public long TimeStamp { get; set; }
+    }
+
+
+    public class GameCacheEntryCreator
+    {
+        public long Id { get; init; }
+
+        public string? Name { get; init; }
     }
 }
