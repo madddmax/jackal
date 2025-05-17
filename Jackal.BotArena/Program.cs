af61aea259b6a4a85bb95f6857d869e5132020cf
madddmax <madddmax@mail.ru>
2025-05-17 09:52:10 +0300

    Доработал расчет статистики для BotArena. Добавил OakioPlayer.

--- Jackal.BotArena/Program.cs
+++ Jackal.BotArena/Program.cs
@@ -7,34 +7,74 @@ using Jackal.Core.Players;
 
 namespace Jackal.BotArena;
 
+/// <summary>
+/// Консольное приложение для оценки силы игроков ботов
+/// </summary>
 internal static class Program
 {
-    private class ArenaStat
+    /// <summary>
+    /// Игровая статистика
+    /// </summary>
+    private class GameStat
     {
+        /// <summary>
+        /// Количество побед, когда в конце игры золота оказалось больше.
+        /// В случае равенства по золоту, победа присуждается обеим командам.
+        /// </summary>
         public int TotalWin { get; set; }
+        
+        /// <summary>
+        /// Суммарное количество добытых монет за все игры
+        /// </summary>
         public int TotalCoins { get; set; }
+        
+        /// <summary>
+        /// Количество проведенных игр
+        /// </summary>
+        public int GamesCount { get; set; }
+
+        /// <summary>
+        /// Среднее количество побед за все игры
+        /// </summary>
+        public double AverageWin => (double)TotalWin / GamesCount;
+        
+        /// <summary>
+        /// Среднее количество добытых монет за все игры
+        /// </summary>
+        public double AverageCoins => (double)TotalCoins / GamesCount;
     }
 
-    private static readonly Dictionary<string, ArenaStat> BotStat = new();
+    /// <summary>
+    /// Статистика по каждому игроку боту
+    /// </summary>
+    private static readonly Dictionary<string, GameStat> BotStat = new();
     
     private static void Main(string[] args)
     {
         IPlayer[][] combinationOfPlayers =
         [
             [
+                new EasyPlayer(),
+                new RandomPlayer()
+            ],
+            [
                 new RandomPlayer(),
-                new EasyPlayer()
+                new EasyPlayer(),
             ],
             [
                 new EasyPlayer(),
-                new RandomPlayer()
+                new OakioPlayer(),
+            ],
+            [
+                new OakioPlayer(),
+                new EasyPlayer()
             ]
         ];
 
-        int gamesCount = 100;
+        int totalGamesCount = 100;
         int gameNumber = 0;
         
-        while (gameNumber < gamesCount)
+        while (gameNumber < totalGamesCount)
         {
             var mapId = new Random().Next();
             var mapSize = 13;
@@ -65,22 +105,28 @@ internal static class Program
         {
             if (!BotStat.TryGetValue(team.Name, out var stat))
             {
-                stat = new ArenaStat();
+                stat = new GameStat();
                 BotStat.Add(team.Name, stat);
             }
             stat.TotalWin += team.Coins == maxCoins ? 1 : 0;
             stat.TotalCoins += team.Coins;
+            stat.GamesCount += 1;
         }
     }
 
     private static void ShowStat(int gameNumber)
     {
-        Console.WriteLine($"Games count = {gameNumber}");
-        var orderedBotStat = BotStat.OrderByDescending(p => p.Value.TotalWin);
-        foreach (var (botName, arenaStat) in orderedBotStat)
+        Console.WriteLine($"Total games count = {gameNumber}");
+        var orderedBotStat = BotStat.OrderByDescending(p => p.Value.AverageWin);
+        foreach (var (botName, gameStat) in orderedBotStat)
         {
             Console.WriteLine(
-                $"Bot name = {botName} | Total win = {arenaStat.TotalWin} | Total coins = {arenaStat.TotalCoins}"
+                $"Bot name = {botName} | " +
+                $"Games count = {gameStat.GamesCount} | " +
+                $"Average win = {gameStat.AverageWin:P} | " +
+                $"Average coins = {gameStat.AverageCoins:F} | " +
+                $"Total win = {gameStat.TotalWin} | " +
+                $"Total coins = {gameStat.TotalCoins}"
             );
         }
     }
