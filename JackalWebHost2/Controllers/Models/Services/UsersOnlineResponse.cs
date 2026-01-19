ea740af7eeeb22e17b7805421645d212ae140425
madddmax <madddmax@mail.ru>
2026-01-19 23:30:33 +0300

    Revert "информация о подключённых пользователях"
    This reverts commit 961d16d0a5b350f761123c396de991086da23360.

--- JackalWebHost2/Controllers/Models/Services/UsersOnlineResponse.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace JackalWebHost2.Controllers.Models.Services
-{
-    public record UsersOnlineResponse(
-        List<long> Users);
-}
--- JackalWebHost2/Interfaces/IUsersOnlineService.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace JackalWebHost2.Interfaces
-{
-    public interface IUsersOnlineService
-    {
-        List<long> AddUser(long userId);
-
-        List<long> RemoveUser(long userId);
-    }
-}
