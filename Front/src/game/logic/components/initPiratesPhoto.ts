47131c6f8430a3af6bab5eb17eea5efe5fcf5efb
LUCK\Mike <mifonrule@gmail.com>
2026-02-05 20:44:02 +0300

    рефакторинг

---
+++ Front/src/game/logic/components/initPiratesPhoto.ts
@@ -0,0 +1,42 @@
+import { Constants } from '/app/constants';
+import { getAnotherRandomValue } from '/app/global';
+
+export interface InitPiratesPhotoProps {
+    girlType: string;
+    allGirls?: GamePirate[];
+    teamId: number;
+    teamGroup: TeamGroup;
+}
+
+export interface PiratePhotoDto {
+    photo: string;
+    photoId: number;
+}
+
+export const InitPiratesPhoto = ({ girlType, allGirls, teamId, teamGroup }: InitPiratesPhotoProps): PiratePhotoDto => {
+    let pname;
+    let pnumber;
+    let extension = '.png';
+
+    if (girlType == Constants.pirateTypes.BenGunn) {
+        pname = 'commonganns/gann';
+        pnumber = getAnotherRandomValue(
+            Constants.gannPhotos,
+            allGirls
+                ?.filter((pr) => pr.type == Constants.pirateTypes.BenGunn && pr.photoId > 0)
+                .map((pr) => pr.photoId) ?? [],
+        );
+    } else if (girlType == Constants.pirateTypes.Friday) {
+        pname = 'commonfridays/friday';
+        pnumber = getAnotherRandomValue(Constants.fridayPhotos, []);
+    } else {
+        pname = `${teamGroup.id}/pirate`;
+        pnumber = getAnotherRandomValue(
+            teamGroup.photos,
+            allGirls?.filter((pr) => pr.teamId == teamId && pr.photoId > 0).map((pr) => pr.photoId) ?? [],
+        );
+        extension = teamGroup.extension || '.png';
+    }
+
+    return { photo: `${pname}_${pnumber.origin}${extension}`, photoId: pnumber.type };
+};
