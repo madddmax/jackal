d5d19ed5ef632aa2ef2e0450aef32f70b9eab0df
LUCK\Mike <mifonrule@gmail.com>
2026-02-26 23:50:22 +0300

    добавляем таймер

--- Front/src/game/content/components/controls/index.tsx
+++ Front/src/game/content/components/controls/index.tsx
@@ -1,16 +1,60 @@
 import cn from 'classnames';
+import { useEffect, useRef, useState } from 'react';
 import { Alert } from 'react-bootstrap';
-import { PiBeerBottleThin } from 'react-icons/pi';
+import { PiBeerBottleThin, PiCoinsLight, PiTimer } from 'react-icons/pi';
 import { useSelector } from 'react-redux';
 
 import classes from './controls.module.less';
 import { Constants } from '/app/constants';
 import { getGameSettings, getGameStatistics, getTeamScores } from '/game/redux/gameSlice';
+import { TeamScores } from '/game/redux/gameSlice.types';
+
+const initTimes = (ts: TeamScores[] | undefined, mode: string | undefined): Record<number, number> => {
+    const times: Record<number, number> = {};
+    if (mode == Constants.gameModeTypes.TwoPlayersInTeam) {
+        [0, 1].forEach((it) => (times[it] = 0));
+    } else {
+        ts?.forEach((it) => (times[it.teamId] = 0));
+    }
+    return times;
+};
 
 function Controls() {
     const { gameId, mapSize, mapId, tilesPackName, gameMode } = useSelector(getGameSettings);
     const stat = useSelector(getGameStatistics);
     const teamScores = useSelector(getTeamScores);
+
+    const [showTiming, setShowTiming] = useState<boolean>(false);
+    const [timing, setTiming] = useState<number>(0);
+    const [curTeamId, setCurTeamId] = useState<number | undefined>(stat?.currentTeamId);
+    const times = useRef<Record<number, number>>(initTimes(teamScores, gameMode));
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            {
+                stat && !stat.isGameOver && setTiming((prev) => (prev += 1));
+            }
+        }, 1000);
+
+        return () => clearTimeout(timer);
+    }, [timing]);
+
+    useEffect(() => {
+        if (stat?.currentTeamId !== undefined) {
+            setCurTeamId((prev) => {
+                if (prev !== undefined) {
+                    if (gameMode == Constants.gameModeTypes.TwoPlayersInTeam) {
+                        times.current[prev % 2] = timing;
+                        setTiming(times.current[stat.currentTeamId % 2] || 0);
+                    } else {
+                        times.current[prev] = timing;
+                        setTiming(times.current[stat.currentTeamId] || 0);
+                    }
+                }
+                return stat.currentTeamId;
+            });
+        }
+    }, [stat?.currentTeamId]);
 
     return (
         <>
@@ -33,6 +77,19 @@ function Controls() {
                 </div>
                 <div>
                     Номер хода: <span>{stat?.turnNumber}</span>
+                    {showTiming ? (
+                        <PiCoinsLight
+                            size={24}
+                            style={{ float: 'right', cursor: 'pointer' }}
+                            onClick={() => setShowTiming(false)}
+                        />
+                    ) : (
+                        <PiTimer
+                            size={24}
+                            style={{ float: 'right', cursor: 'pointer' }}
+                            onClick={() => setShowTiming(true)}
+                        />
+                    )}
                 </div>
                 {gameMode != Constants.gameModeTypes.TwoPlayersInTeam && teamScores && (
                     <div className={cn(classes.teams, 'container')}>
@@ -55,7 +112,7 @@ function Controls() {
                                     >
                                         <div
                                             style={{
-                                                background: it.teamId == stat?.currentTeamId ? 'gold' : 'white',
+                                                background: it.teamId == curTeamId ? 'gold' : 'white',
                                                 borderRadius: '50rem',
                                             }}
                                         >
@@ -64,9 +121,20 @@ function Controls() {
                                     </div>
                                 </div>
                                 <div className={cn(classes.scores, 'col-4')}>
-                                    <PiBeerBottleThin style={{ marginRight: '2px', color: 'white' }} />
-                                    <span style={{ color: 'white' }}>{it.bottles}</span>
-                                    <div className={cn(classes.coinsCount, 'coins')}>{it.coins}</div>
+                                    {showTiming ? (
+                                        <>
+                                            <PiTimer style={{ marginRight: '2px', color: 'white' }} />
+                                            <span style={{ color: 'white' }}>
+                                                {curTeamId === it.teamId ? timing : times.current[it.teamId]}
+                                            </span>
+                                        </>
+                                    ) : (
+                                        <>
+                                            <PiBeerBottleThin style={{ margin: '0 2px', color: 'white' }} />
+                                            <span style={{ color: 'white' }}>{it.bottles}</span>
+                                            <div className={cn(classes.coinsCount, 'coins')}>{it.coins}</div>
+                                        </>
+                                    )}
                                 </div>
                             </div>
                         ))}
@@ -97,8 +165,7 @@ function Controls() {
                                             >
                                                 <div
                                                     style={{
-                                                        background:
-                                                            scores.teamId == stat?.currentTeamId ? 'gold' : 'white',
+                                                        background: scores.teamId == curTeamId ? 'gold' : 'white',
                                                         borderRadius: '50rem',
                                                     }}
                                                 >
@@ -109,9 +176,22 @@ function Controls() {
                                     })}
                                 </div>
                                 <div className={cn(classes.scores, 'col-4')}>
-                                    <PiBeerBottleThin style={{ marginRight: '2px', color: 'white' }} />
-                                    <span style={{ color: 'white' }}>{teamScores[num].bottles}</span>
-                                    <div className={cn(classes.coinsCount, 'coins')}>{teamScores[num].coins}</div>
+                                    {showTiming ? (
+                                        <>
+                                            <PiTimer style={{ marginRight: '2px', color: 'white' }} />
+                                            <span style={{ color: 'white' }}>
+                                                {(curTeamId ?? 0) % 2 === num ? timing : times.current[num]}
+                                            </span>
+                                        </>
+                                    ) : (
+                                        <>
+                                            <PiBeerBottleThin style={{ marginRight: '2px', color: 'white' }} />
+                                            <span style={{ color: 'white' }}>{teamScores[num].bottles}</span>
+                                            <div className={cn(classes.coinsCount, 'coins')}>
+                                                {teamScores[num].coins}
+                                            </div>
+                                        </>
+                                    )}
                                 </div>
                             </div>
                         ))}
